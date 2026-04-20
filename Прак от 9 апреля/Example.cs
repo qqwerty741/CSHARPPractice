22c11d80e68c4e97ff5aa9623c457c8f539b18e0
qqwerty741 <58818436+qqwerty741@users.noreply.github.com>
2026-04-20 14:36:57 +0300

    Create Example.cs

---
+++ Прак от 9 апреля/Example.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp5
+{
+    public class Example
+    {
+        private int _num;
+        private string _word;
+        private int _rd;
+
+        public Example(int num = -1, string word = "unknown", int rd = 3)
+        {
+            _num = num;
+            _word = word;
+            _rd = rd;
+        }
+    }
+}
